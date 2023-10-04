a2d7327f19885e1498fc5ff5cb7f11635cdf53b5
Aymen <aymenbouazra994@gmail.com>
2023-10-04 17:43:03 +0100

    added searchFormMap

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -15,11 +15,14 @@ export class SearchFormComponent implements OnInit {
   searchFormMap?: FormGroup
   omraType = 'map'
   numAdults = 1
+  numAdultsMap = 1
   numChildren = 0
+  numChildrenMap = 0
   dropdownOpen = false;
   constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
     this.fromDate = calendar.getToday();
     this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+
   }
 
   ngOnInit(): void {
@@ -35,17 +38,29 @@ export class SearchFormComponent implements OnInit {
       })]),
     })
     this.searchFormMap = new FormGroup({
-      residencecountry: new FormControl('', [Validators.required]),
+      fromDate: new FormControl({}),
+      toDate: new FormControl({}),
+      residenceCountry: new FormControl('', [Validators.required]),
       nationality: new FormControl('', [Validators.required]),
       path: new FormControl('', [Validators.required]),
-      passengers: new FormArray([new FormGroup({
+      firstDestination: new FormGroup({
+        destinationOne: new FormControl('', [Validators.required]),
+        nights: new FormControl(0, [Validators.required]),
+      }),
+      secondDestination: new FormGroup({
+        destinationTwo: new FormControl('', [Validators.required]),
+        nights: new FormControl(0, [Validators.required]),
+      }),
+      passengersmap: new FormArray([new FormGroup({
         adults: new FormControl(1),
-        children: new FormArray([]),
+        childrenmap: new FormArray([]),
         room: new FormControl("Room 1"),
       })]),
     })
+    this.searchFormMap?.get('fromDate')?.setValue(this.fromDate)
+    this.searchFormMap?.get('toDate')?.setValue(this.toDate)
   }
-  
+
 
   onChangeType() {
     console.log(this.omraType);
@@ -85,22 +100,19 @@ export class SearchFormComponent implements OnInit {
     const parsed = this.formatter.parse(input);
     return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
   }
-
+  // **************************
+  // Package Section
+  // **************************
   get passengers(): FormArray {
     return this.searchFormPackage?.get('passengers') as FormArray
   }
 
-  get passengersmap(): FormArray {
-    return this.searchFormMap?.get('passengersmap') as FormArray
-  }
 
   children(i: any): FormArray {
     return this.passengers.at(i)?.get('children') as FormArray
   }
 
-  childrenmap(i: any): FormArray {
-    return this.passengersmap.at(i)?.get('childrenmap') as FormArray
-  }
+
 
   addPassenger() {
     if (this.passengers.length < 3) {
@@ -108,17 +120,6 @@ export class SearchFormComponent implements OnInit {
         adults: new FormControl(1),
         children: new FormArray([]),
         room: new FormControl("Room " + (this.passengers.length + 1)),
-      }))
-      this.numAdults += 1
-    }
-  }
-
-  addPassengermap() {
-    if (this.passengersmap.length < 3) {
-      this.passengersmap.push(new FormGroup({
-        adults: new FormControl(1),
-        children: new FormArray([]),
-        room: new FormControl("Room " + (this.passengersmap.length + 1)),
       }))
       this.numAdults += 1
     }
@@ -132,13 +133,7 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  deletePassengermap() {
-    if (this.passengersmap.length > 1) {
-      this.numAdults = this.numAdults - this.passengersmap.at(this.passengersmap.length - 1).get('adults')?.value
-      this.numChildren = this.numChildren - this.passengersmap.at(this.passengersmap.length - 1).get('children')?.value.length
-      this.passengersmap.removeAt(this.passengersmap.length - 1)
-    }
-  }
+
 
   pluschildren(i: any) {
     if (this.children(i).length < 4) {
@@ -149,14 +144,7 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  pluschildrenmap(i: any) {
-    if (this.childrenmap(i).length < 4) {
-      this.childrenmap(i).push(new FormGroup({
-        age: new FormControl("")
-      }))
-      this.numChildren += 1
-    }
-  }
+
 
   minuschildren(i: any) {
     if (this.children(i).length > 0) {
@@ -165,12 +153,7 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  minuschildrenmap(i: any) {
-    if (this.childrenmap(i).length > 0) {
-      this.childrenmap(i).removeAt(this.childrenmap(i).length - 1)
-      this.numChildren -= 1
-    }
-  }
+
 
   plusadults(i: any) {
     if (this.passengers.at(i).get('adults')?.value < 5) {
@@ -179,12 +162,7 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  plusadultsmap(i: any) {
-    if (this.passengersmap.at(i).get('adultsmap')?.value < 5) {
-      this.passengersmap.at(i).get('adultsmap')?.setValue(this.passengersmap.at(i).get('adultsmap')?.value + 1)
-      this.numAdults += 1
-    }
-  }
+
 
   minusadults(i: any) {
     if (this.passengers.at(i).get('adults')?.value > 1) {
@@ -193,14 +171,108 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  minusadultsmap(i: any) {
-    if (this.passengersmap.at(i).get('adultsmap')?.value > 1) {
-      this.passengersmap.at(i).get('adultsmap')?.setValue(this.passengersmap.at(i).get('adultsmap')?.value - 1)
-      this.numAdults -= 1
-    }
-  }
 
   searchPackage() {
     console.log(this.searchFormPackage?.value);
   }
+
+  // **************************
+  // Map Section
+  // **************************
+  get passengersmap(): FormArray {
+    return this.searchFormMap?.get('passengersmap') as FormArray
+  }
+  childrenmap(i: any): FormArray {
+    return this.passengersmap.at(i)?.get('childrenmap') as FormArray
+  }
+
+  get firstDestination(): FormGroup {
+    return this.passengersmap.get('firstDestination') as FormGroup
+  }
+  get secondDestination(): FormGroup {
+    return this.passengersmap.get('secondDestination') as FormGroup
+  }
+  addPassengermap() {
+    if (this.passengersmap.length < 3) {
+      this.passengersmap.push(new FormGroup({
+        adults: new FormControl(1),
+        childrenmap: new FormArray([]),
+        room: new FormControl("Room " + (this.passengersmap.length + 1)),
+      }))
+      this.numAdults += 1
+    }
+  }
+  deletePassengermap() {
+    if (this.passengersmap.length > 1) {
+      this.numAdults = this.numAdults - this.passengersmap.at(this.passengersmap.length - 1).get('adults')?.value
+      this.numChildrenMap = this.numChildrenMap - this.passengersmap.at(this.passengersmap.length - 1).get('childrenmap')?.value.length
+      this.passengersmap.removeAt(this.passengersmap.length - 1)
+    }
+  }
+
+  plusNightsFirstDestionation() {
+    const nightsFormControl = this.searchFormMap?.get('firstDestination')?.get('nights');
+    if (nightsFormControl) {
+      let currentNightsValue = nightsFormControl.value;
+      currentNightsValue += 1;
+      nightsFormControl.setValue(currentNightsValue);
+    }
+  }
+  minusNightsFirstDestionation() {
+    const nightsFormControl = this.searchFormMap?.get('firstDestination')?.get('nights');
+    if (nightsFormControl && nightsFormControl.value > 0) {
+      let currentNightsValue = nightsFormControl.value;
+      currentNightsValue -= 1;
+      nightsFormControl.setValue(currentNightsValue);
+    }
+  }
+
+  plusNightsSecondDestionation() {
+    const nightsFormControl = this.searchFormMap?.get('secondDestination')?.get('nights');
+    if (nightsFormControl) {
+      let currentNightsValue = nightsFormControl.value;
+      currentNightsValue += 1;
+      nightsFormControl.setValue(currentNightsValue);
+    }
+  }
+  minusNightsSecondDestionation() {
+    const nightsFormControl = this.searchFormMap?.get('secondDestination')?.get('nights');
+    if (nightsFormControl && nightsFormControl.value > 0) {
+      let currentNightsValue = nightsFormControl.value;
+      currentNightsValue -= 1;
+      nightsFormControl.setValue(currentNightsValue);
+    }
+  }
+
+  pluschildrenmap(i: any) {
+    if (this.childrenmap(i).length < 4) {
+      this.childrenmap(i).push(new FormGroup({
+        age: new FormControl("")
+      }))
+      this.numChildrenMap += 1
+    }
+  }
+  minuschildrenmap(i: any) {
+    if (this.childrenmap(i).length > 0) {
+      this.childrenmap(i).removeAt(this.childrenmap(i).length - 1)
+      this.numChildrenMap -= 1
+    }
+  }
+  plusadultsmap(i: any) {
+    if (this.passengersmap.at(i).get('adults')?.value < 5) {
+      this.passengersmap.at(i).get('adults')?.setValue(this.passengersmap.at(i).get('adults')?.value + 1)
+      this.numAdultsMap += 1
+    }
+  }
+  minusadultsmap(i: any) {
+    if (this.passengersmap.at(i).get('adults')?.value > 1) {
+      this.passengersmap.at(i).get('adults')?.setValue(this.passengersmap.at(i).get('adults')?.value - 1)
+      this.numAdultsMap -= 1
+    }
+  }
+
+  searchMap() {
+    console.log(this.searchFormMap?.value);
+  }
+
 }
