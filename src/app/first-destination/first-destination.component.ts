3095bca372b6113e737741452a170a988429906b
Rachoud32 <rachoud32@gmail.com>
2023-10-09 08:56:13 +0100

    creating 1st and 2nd destination pages

--- src/app/first-destination/first-destination.component.ts
+++ src/app/first-destination/first-destination.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPlane, faArrowRightLong, faCaretRight, faMagnifyingGlass, faInfoCircle, faTag, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-first-destination',
@@ -6,12 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-destination.component.css']
 })
 export class FirstDestinationComponent {
-  selectedOption: string = '';
+  faPlane = faPlane
+  faArrowRightLong = faArrowRightLong
+  faCaretRight = faCaretRight
+  faMagnifyingGlass = faMagnifyingGlass
+  faInfoCircle = faInfoCircle
+  faTag = faTag
+  faChevronLeft = faChevronLeft
+  faChevronRight = faChevronRight
+  
+  selectedOption: string = 'option1';
   option1 = 'option1';
   option2 = 'option2';
-
+  option3 = 'option3';
+  option4 = 'option4';
   checkOption() {
-    this.selectedOption = this.option1; // Set the selected option to option1
+    this.selectedOption = this.option1;
+  }
+  checkOption2() {
+    this.selectedOption = this.option2;
+  }
+  checkOption3() {
+    this.selectedOption = this.option3;
+  }
+  checkOption4() {
+    this.selectedOption = this.option4;
   }
 }
 
