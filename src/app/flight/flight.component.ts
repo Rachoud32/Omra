e615a259bf130bf265963d13269797c3206a3529
Aymen <aymenbouazra994@gmail.com>
2023-10-14 02:24:37 +0100

    select flight

--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -11,25 +11,13 @@ export class FlightComponent implements OnInit {
   faArrowRightLong = faArrowRightLong
   faCaretRight = faCaretRight
 
-  selectedOption: string = 'option1';
-  option1 = 'option1';
-  option2 = 'option2';
-  option3 = 'option3';
-  option4 = 'option4';
-  checkOption() {
-    this.selectedOption = this.option1;
+  selectedFlight: string = 'flightbox1';
+  checkOption(value: string) {
+    this.selectedFlight = value;
   }
-  checkOption2() {
-    this.selectedOption = this.option2;
-  }
-  checkOption3() {
-    this.selectedOption = this.option3;
-  }
-  checkOption4() {
-    this.selectedOption = this.option4;
-  }
+
   ngOnInit(): void {
-   
+
   }
 }
 
