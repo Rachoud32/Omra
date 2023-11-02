fd0c9d3c0736e8fb8653305a7ce720c48c2891af
Rachoud32 <rachoud32@gmail.com>
2023-11-02 20:01:19 +0100

    change icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'umrah-project';
+  loadingRouteConfig: boolean = false;
 
+  constructor (private router: Router) {}
+
+  ngOnInit () {
+    this.router.events.subscribe(event => {
+        if (event instanceof RouteConfigLoadStart) {
+            this.loadingRouteConfig = true;
+        } else if (event instanceof RouteConfigLoadEnd) {
+            this.loadingRouteConfig = false;
+        }
+    });
+  }
 }
